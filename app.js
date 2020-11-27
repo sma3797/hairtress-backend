0cf479a87270224d5274ac3e2b9e01d12e5d8bc5
Muzammil <sma3797@gmail.com>
2020-11-27 19:32:40 +0500

    1

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ mongoose
     )
     .then((result) => {
         console.log("Yo");
-        app.listen(process.env.PORT || 5000);
+        app.listen(process.env.PORT || 5003);
     })
     .catch((err) => {
         console.log("Error", err);
